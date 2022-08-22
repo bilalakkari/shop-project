2528f564649c78561f7940a7805dd7592ab6cf74
unknown <bilalakkari>
2022-08-22 15:02:30 +0300

    ready to deploy

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -9,7 +9,7 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getProduct() {
-    return this.http.get<any>("https://ma20222208.herokuapp.com/api/home/data")
+    return this.http.get<any>("https://product-api-nodejs.herokuapp.com/products/")
       .pipe(map((res: any) => {
         return res;
       }))
